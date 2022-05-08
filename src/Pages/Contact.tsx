a1d160b30ef3e833d81986ee73d73457f060b280
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-05-08 12:36:54 +0400

    form header styling

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,7 +1,24 @@
+import { FaQuestion } from 'react-icons/fa';
+
 export const CONTACT_US = () => {
   return (
     <section className="contact_us_container">
-      
+      <h1>Contact Us</h1>
+      <form action="" className="form_wrapper">
+        <header className="form_header_wrapper">
+          <div>
+            <span>
+              Ask A Question
+            </span>
+            <span>
+              Please contact us for specific questions
+            </span>
+          </div>
+          <div>
+            <FaQuestion />
+          </div>
+        </header>
+      </form>
     </section>
   );
 };
