480e6b3b79d8adb7c0863a1a35a0b5517a3bea8e
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-05-12 15:58:13 +0400

    sign in optioins, username/email and password for login

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,67 +1,71 @@
-import { useState } from 'react';
-import { SIGNUP_INPUTS_IF, SIGNIN_INPUTS_IF, SIGNIN_VALUES_IF } from '../Types';
+import { useState } from "react";
+import { SIGNUP_INPUTS_IF, SIGNIN_INPUTS_IF, SIGNIN_VALUES_IF } from "../Types";
 
 export const SIGN_IN = () => {
   const [values, setValues] = useState<SIGNIN_VALUES_IF>({
-    email: '',
-    phone_number: '',
-    first_name: '',
-    last_name: '',
-    password: '',
-    confirm_password: ''
+    email: "",
+    phone_number: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    confirm_password: ""
   });
 
-  const signup_inputs: SIGNUP_INPUTS_IF[]  = [
+  const signup_inputs: SIGNUP_INPUTS_IF[] = [
     {
       id: 1,
-      name: 'email',
-      type: 'email',
-      placeholder: 'email@email.com'
+      name: "email",
+      type: "email",
+      placeholder: "email@email.com"
     },
     {
-      id: 2, 
-      name: 'phone_number',
-      type: 'tel',
-      placeholder: '+263 77 878 923'
+      id: 2,
+      name: "phone_number",
+      type: "tel",
+      placeholder: "+263 77 878 923"
     },
     {
-      id: 3, 
-      name: 'first_name',
-      type: 'text',
-      placeholder: 'first name'
+      id: 3,
+      name: "first_name",
+      type: "text",
+      placeholder: "first name"
     },
-    { 
+    {
       id: 4,
-      name: 'last_name',
-      type: 'text',
-      placeholder: 'last name'
-    },
+      name: "last_name",
+      type: "text",
+      placeholder: "last name"
+    }
   ];
 
   const signin_inputs: SIGNIN_INPUTS_IF[] = [
     {
-      id:1,
-      name: 'email',
-      type: 'email',
-      placeholder: 'email',
+      id: 1,
+      name: "email",
+      type: "email",
+      placeholder: "email"
     },
     {
-      id:2, 
-      name: 'password',
-      type: 'password',
-      placeholder: 'password'
-    },
-  ]
+      id: 2,
+      name: "password",
+      type: "password",
+      placeholder: "password"
+    }
+  ];
 
   return (
     <div className="sign_in_container">
       <form action="" className="sign_in_form_wrapper">
         <main className="main_sign_in">
-          
+          {signin_inputs.map((input) => (
+            <input
+              key={input.id}
+              name={input.name}
+              type={input.type}
+              placeholder={input.placeholder}
+            />
+          ))}
         </main>
-        <aside className="aside_sign_up">
-
-        </aside>
       </form>
     </div>
   );
